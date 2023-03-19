4c76a2f51500f1ba055faa2eff24246307b5e52e
LT Tsai <and.liting@gmail.com>
2023-03-19 20:04:31 +0800

    css test

--- src/template/intro.clj
+++ src/template/intro.clj
@@ -1,4 +1,14 @@
 (ns template.intro)
 
 (defn template-s []
-  [:h1 "Welcome to NorthBy"])
+  [:html {:lang "en"}
+   [:head
+    [:link {:rel "stylesheet" :href "/css/output.css"}]]
+   [:body
+    [:div {:class "flex justify-center bg-gray-300"}
+     [:div {:class "mx-4 order-last"}
+      [:img {:src "images/music.svg" :size "100x100"}]]
+     [:div {:class "mx-4 self-center text-center"}
+      [:h1 {:class "text-6xl font-bold text-blue-700"} "Welcome to NorthBy"]
+      [:h2 {:class "text-3xl font-semibold text-blue-300"} "A premium in sight and sound"]
+      [:button {:class "my-4 px-4 py-2 border-2 border-black rounded-lg text-white bg-blue-900"} "Learn More"]]]]])
