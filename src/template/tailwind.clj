909235ba3740c4f804e5cb3e07c4568bbe598505
LT Tsai <and.liting@gmail.com>
2023-04-04 13:50:47 +0800

    update css

--- src/template/tailwind.clj
+++ src/template/tailwind.clj
@@ -3,3 +3,6 @@
 
 (defn index-header []
   "text-3xl font-bold mt-12 mb-6 font-serif")
+
+(defn nav-a []
+  "text-black font-bold text-lg no-underline uppercase")
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: [
     "./src/template/*.clj"
   ],
+  safelist: [
+    ".header-section-number",
+    ".toc-section-number"
+  ],
   theme: {
     extend: {},
   },
