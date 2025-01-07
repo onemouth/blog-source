42f92c4a6e2f5929a0df18e1d51b0fb7f5e6ebfa
LT Tsai <and.liting@gmail.com>
2025-01-07 20:43:07 +0800

    remove giscue

--- src/template/post.clj
+++ src/template/post.clj
@@ -20,19 +20,6 @@
          "$body$"]]
        "$body$")]]
    [:comment
-    [:script {:src "https://giscus.app/client.js"
-              :data-repo "onemouth/blog-source"
-              :data-repo-id "MDEwOlJlcG9zaXRvcnkyNjExMjI0MTE="
-              :data-category "General"
-              :data-category-id "DIC_kwDOD5Bpa84COAIg"
-              :data-mapping "pathname"
-              :data-reactions-enabled "0"
-              :data-emit-metadata "0"
-              :data-input-position "top"
-              :data-theme "light"
-              :data-lang "en"
-              :crossorigin "anonymous"
-              :async true}]
     [:script {:type "text/javascript"
               :src "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML"
               :async true}]]])
